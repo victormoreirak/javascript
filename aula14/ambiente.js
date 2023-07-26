9a0a963a7fb2084b0b739b6070ee75295659f20f
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-07-26 15:50:11 -0300

    parte 5 exercicios modelo e

---
+++ aula14/ambiente.js
@@ -0,0 +1,30 @@
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('FIM')
+
+
+/*
+for(var c =1; c <= 5; c++) {
+    console.log(c)
+}
+*/
+
+
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <=5)
+*/
+
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
